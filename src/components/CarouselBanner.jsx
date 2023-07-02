2d9eaa5254df1427358dcbe0421f71867ce47252
heartdoc516 <heartdoc855@gmail.com>
2023-07-02 14:23:15 -0400

    carousel banner text block

--- src/components/CarouselBanner.jsx
+++ src/components/CarouselBanner.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { tmdbApiToken } from "../utils/tmdbToken.js";
-import img from "../assets/john-wick.jpg";
 import "../style/carouselbanner.css";
+
+function getGenreNames(genreIdArray, genres) {
+  let genreNames = [];
+  genres.forEach((genre) => {
+    if (genreIdArray.includes(genre.id)) {
+      genreNames.push(genre.name);
+    }
+  });
+  return genreNames;
+}
 
 const CarouselBanner = () => {
   const [data, setData] = useState([]);
   const [active, setActive] = useState("");
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     const url =
@@ -29,21 +39,51 @@ const CarouselBanner = () => {
       .catch((err) => console.error("error:" + err));
   }, []);
 
+  useEffect(() => {
+    const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${tmdbApiToken}`,
+      },
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => {
+        setGenres(json.genres);
+      })
+      .catch((err) => console.error("error:" + err));
+  }, []);
+
   return (
     <div id="carouselExample" className="carousel-banner carousel slide">
       <div className="carousel-inner">
         {data.map((item) => (
           <div className={`carousel-item ${active.id === item.id && "active"}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
-              className="d-block"
-              alt="..."
-            />
+            <div className="img-container">
+              <img
+                src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+                className="d-block"
+                alt="..."
+              />
+              <div className="text-block">
+                <h1 className="title text-white">{item.original_title}</h1>
+                <div>{}</div>
+                <button>Watch Trailer</button>
+                <div>
+                  {getGenreNames(item.genre_ids, genres).map((genre) => (
+                    <div className="text-white">{genre}</div>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
       <button
-        className="carousel-control-prev align-items-end"
+        className="carousel-control-prev align-items-center"
         type="button"
         data-bs-target="#carouselExample"
         data-bs-slide="prev"
@@ -55,7 +95,7 @@ const CarouselBanner = () => {
         <span className="visually-hidden">Previous</span>
       </button>
       <button
-        className="carousel-control-next align-items-end"
+        className="carousel-control-next align-items-center"
         type="button"
         data-bs-target="#carouselExample"
         data-bs-slide="next"
